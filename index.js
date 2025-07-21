0d2a9356740aae682316a8136a4b6b6317e33146
amitpaul2004 <pal900290@gmail.com>
2025-07-21 12:13:26 +0530

    Your commit message

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(`Date of Birth: 24 November 2004
+Gender: Male
+Email: pal900290@gmail.com
+Phone: +91 9836868192
+Nationality: Indian
+Languages Known: English, Hindi, Bengali
+LinkedIn: linkedin.com/in/amitpaul
+GitHub: github.com/amitpaul2004`);

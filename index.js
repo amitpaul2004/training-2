073c3756c74a611f7837377d8e453694d2d65ece
amitpaul2004 <pal900290@gmail.com>
2025-07-21 12:38:49 +0530

    Your commit message

--- index.js
+++ index.js
@@ -6,3 +6,37 @@ Nationality: Indian
 Languages Known: English, Hindi, Bengali
 LinkedIn: linkedin.com/in/amitpaul
 GitHub: github.com/amitpaul2004`);
+
+var a = 10;
+var b = 20;
+
+var sum = a + b;
+var sub = a - b;
+var mul = a * b;
+var div = a / b;
+
+console.log(`Sum of two numbers is ${sum}`);
+console.log(`Subtraction of two numbers is ${sub}`);
+console.log(`Multiplication of two numbers is ${mul}`);
+console.log(`Division of two numbers is ${div}`);
+
+// Triangle area (0.5 * base * height)
+var base = 5;
+var height = 6;
+var triangleArea = 0.5 * base * height;
+console.log(`Area of Triangle is ${triangleArea}`);
+
+// Circle area (π * r^2)
+var radius = 7;
+var circleArea = Math.PI * radius * radius;
+console.log(`Area of Circle is ${circleArea.toFixed(2)}`);
+
+// Cube surface area (6 * side^2)
+var sideCube = 4;
+var cubeArea = sideCube * sideCube * sideCube;
+console.log(`Surface Area of Cube is ${cubeArea}`);
+
+// Square area (side^2)
+var sideSquare = 5;
+var squareArea = sideSquare * sideSquare;
+console.log(`Area of Square is ${squareArea}`);
